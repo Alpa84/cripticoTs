829d95b1488060fc3efdfe586d3b646387c16563
alejandro-panza <alejandrofpanza@gmail.com>
2020-05-15 15:27:11 -0300

    better mobile tour

--- src/components/TourWrapper.tsx
+++ src/components/TourWrapper.tsx
@@ -38,16 +38,16 @@ function TourWrapper({children, functions, general, tutName, componentClass}: Pr
       data-tut={tutName}>
       { show && (
         <>
-          <div className='closeButton'>
-            <svg
-              className='tourArrow'
-              type="button"
-              onClick={() => functions.dispatch({ type: 'changeMobileTourOpen', on: false })}
-              viewBox="0 0 9.1 9.1">
-              <path fill="currentColor" d="M5.9 4.5l2.8-2.8c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L4.5 3.1 1.7.3C1.3-.1.7-.1.3.3c-.4.4-.4 1 0 1.4l2.8 2.8L.3 7.4c-.4.4-.4 1 0 1.4.2.2.4.3.7.3s.5-.1.7-.3L4.5 6l2.8 2.8c.3.2.5.3.8.3s.5-.1.7-.3c.4-.4.4-1 0-1.4L5.9 4.5z" />
-            </svg>
-          </div>
           <div className={doFlash ? 'tourExplanation tourFlash' : 'tourExplanation'}>
+            <div className='closeButton'>
+              <svg
+                className='tourArrow'
+                type="button"
+                onClick={() => functions.dispatch({ type: 'changeMobileTourOpen', on: false })}
+                viewBox="0 0 9.1 9.1">
+                <path fill="currentColor" d="M5.9 4.5l2.8-2.8c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L4.5 3.1 1.7.3C1.3-.1.7-.1.3.3c-.4.4-.4 1 0 1.4l2.8 2.8L.3 7.4c-.4.4-.4 1 0 1.4.2.2.4.3.7.3s.5-.1.7-.3L4.5 6l2.8 2.8c.3.2.5.3.8.3s.5-.1.7-.3c.4-.4.4-1 0-1.4L5.9 4.5z" />
+              </svg>
+            </div>
             {step.content()}
             <div className='tourArrows'>
               {general.mobileStep > 0 && (
